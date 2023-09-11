e736925fe43aa9b11652fd9854babb64ee9acffe
jykim-rust <alexj99@naver.com>
2023-09-11 19:39:53 +0900

    [JYK] 전역 Exception 처리를 위해, CustomException과 그냥 Exception을 각각 처리해 줄 GlobalExceptionHandler 추가

---
+++ src/main/java/com/thewayhome/project/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.thewayhome.project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.ResponseErrorHandler;
+
+
+@RestControllerAdvice(annotations = RestController.class)
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    protected ResponseEntity<Object> handleUnknownException(Exception e){
+        return new ResponseEntity<>("UnknownException : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<Object> handleCustomException(CustomException e){
+        return new ResponseEntity<>(e.getCustomError().getMessage(), HttpStatusCode.valueOf(e.getCustomError().getStatus()));
+    }
+}
