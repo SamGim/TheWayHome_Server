1e5ef632d022b9e886ed19588661f4bd42370f97
jykim-rust <alexj99@naver.com>
2023-09-11 19:41:11 +0900

    [JYK] 서버의 생사를 확인하는 /heartbeat 추가 및 CustomException 테스트

---
+++ src/main/java/com/thewayhome/project/controller/RootController.java
@@ -0,0 +1,24 @@
+package com.thewayhome.project.controller;
+
+
+import com.thewayhome.project.exception.CustomError;
+import com.thewayhome.project.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.thewayhome.project.exception.CustomError.TEST_ERROR;
+
+@Slf4j
+@RestController
+//@RequestMapping("/heartbeat")
+public class RootController {
+
+    @GetMapping("/heartbeat")
+    public ResponseEntity<Object> isServerAlive(){
+        throw new CustomException(TEST_ERROR);
+//        return ResponseEntity.ok("connected");
+    }
+}
