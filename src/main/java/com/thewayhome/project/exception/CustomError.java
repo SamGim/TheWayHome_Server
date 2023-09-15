550ff9cfdd9adc5ff420b52bfae6f21f5cab3625
jykim-rust <alexj99@naver.com>
2023-09-15 21:27:26 +0900

    [JYK] Custom error 추가

--- src/main/java/com/thewayhome/project/exception/CustomError.java
+++ src/main/java/com/thewayhome/project/exception/CustomError.java
@@ -6,7 +6,11 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum CustomError {
-    TEST_ERROR(404, "테스트 에러입니다.");
+    TEST_ERROR(404, "테스트 에러입니다."),
+    NO_DATA_ERROR(404, "존재하지 않습니다."),
+    DB_SAVE_ERROR(404,"데이터베이스에 저장하지 못했습니다.");
+
+
     private final int status;
     private final String message;
 }
