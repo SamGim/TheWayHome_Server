9888cc24628c48e7d3cbafb467b0f9f71c5911b7
jykim-rust <alexj99@naver.com>
2023-09-15 21:25:22 +0900

    [JYK] Company 서비스 추가

---
+++ src/main/java/com/thewayhome/project/service/CompanyService.java
@@ -0,0 +1,42 @@
+package com.thewayhome.project.service;
+
+import com.thewayhome.project.domain.Company;
+import com.thewayhome.project.dto.company.CompanyGetFullNameResponseDto;
+import com.thewayhome.project.dto.company.CompanyRegisterRequestDto;
+import com.thewayhome.project.exception.CustomError;
+import com.thewayhome.project.exception.CustomException;
+import com.thewayhome.project.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CompanyService {
+    @Autowired
+    public CompanyService(CompanyRepository companyRepository){
+        this.companyRepository = companyRepository;
+    }
+    private final CompanyRepository companyRepository;
+
+    public Page<CompanyGetFullNameResponseDto> searchCompanyNameByPart(Pageable page, String part) {
+        return companyRepository.searchCompanyByPartName(page, part);
+    }
+
+    public Company registerCompany(CompanyRegisterRequestDto companyDto) {
+        // CompanyDTO에서 필요한 데이터를 가져와서 Company 엔티티로 변환
+        Company company = companyDto.toEntity();
+        // 회사 등록
+        try{
+            return companyRepository.save(company);
+        } catch (Exception e) {
+            throw new CustomException(CustomError.DB_SAVE_ERROR);
+        }
+    }
+
+    public Optional<Company> getCompanyById(Long id) {
+        return companyRepository.findById(id);
+    }
+}
