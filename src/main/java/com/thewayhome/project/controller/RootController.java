2e7712879b8c21970e08891482c14cb16ec09200
jykim-rust <alexj99@naver.com>
2023-09-15 21:27:41 +0900

    [JYK] 테스트용

--- src/main/java/com/thewayhome/project/controller/RootController.java
+++ src/main/java/com/thewayhome/project/controller/RootController.java
@@ -1,11 +1,17 @@
 package com.thewayhome.project.controller;
 
 
+import com.thewayhome.project.domain.Complex;
 import com.thewayhome.project.exception.CustomException;
+import com.thewayhome.project.service.ComplexService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import static com.thewayhome.project.exception.CustomError.TEST_ERROR;
 
@@ -13,10 +19,24 @@ import static com.thewayhome.project.exception.CustomError.TEST_ERROR;
 @RestController
 //@RequestMapping("/heartbeat")
 public class RootController {
+    private final ComplexService complexService;
 
+    @Autowired
+    public RootController(ComplexService complexService) {
+        this.complexService = complexService;
+    }
     @GetMapping("/heartbeat")
     public ResponseEntity<Object> isServerAlive(){
         throw new CustomException(TEST_ERROR);
 //        return ResponseEntity.ok("connected");
+    }
+
+    @GetMapping("/complex")
+    public List<Complex> getComplexesWithinBoundingBox(
+            @RequestParam(name = "sw_lng") double swLng,
+            @RequestParam(name = "sw_lat") double swLat,
+            @RequestParam(name = "ne_lng") double neLng,
+            @RequestParam(name = "ne_lat") double neLat) {
+        return complexService.getComplexesInBoundingBox(swLng, swLat, neLng, neLat);
     }
 }
