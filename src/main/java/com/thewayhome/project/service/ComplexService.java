c5381d8da4af9bf6aec6a0379f5830ccf66df912
jykim-rust <alexj99@naver.com>
2023-09-17 14:56:36 +0900

    [JYK] 단일 매물 정보 가져오는 메소드 두개 추가

--- src/main/java/com/thewayhome/project/service/ComplexService.java
+++ src/main/java/com/thewayhome/project/service/ComplexService.java
@@ -1,11 +1,17 @@
 package com.thewayhome.project.service;
 
 import com.thewayhome.project.domain.Complex;
+import com.thewayhome.project.dto.complex.ComplexCardRequestDto;
+import com.thewayhome.project.dto.complex.ComplexDetailRequestDto;
+import com.thewayhome.project.dto.complex.ComplexSimpleRequestDto;
+import com.thewayhome.project.exception.CustomError;
+import com.thewayhome.project.exception.CustomException;
 import com.thewayhome.project.repository.ComplexRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ComplexService {
@@ -16,7 +22,20 @@ public class ComplexService {
         this.complexRepository = complexRepository;
     }
 
-    public List<Complex> getComplexesInBoundingBox(double swLng, double swLat, double neLng, double neLat) {
-        return complexRepository.findWithinMap(swLng, swLat, neLng, neLat);
+    public List<ComplexSimpleRequestDto> getComplexesInBoundingBox(double swLng, double swLat, double neLng, double neLat) {
+        List<Complex> withinMap = complexRepository.findWithinMap(swLng, swLat, neLng, neLat);
+
+        return withinMap.stream()
+                .map(ComplexSimpleRequestDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public ComplexCardRequestDto getComplexCardInfo(Long id){
+        Complex complex = complexRepository.findById(id).orElseThrow(() -> new CustomException(CustomError.WRONG_ID_ERROR));
+        return ComplexCardRequestDto.fromEntity(complex);
+    }
+    public ComplexDetailRequestDto getComplexDetailInfo(Long id){
+        Complex complex = complexRepository.findById(id).orElseThrow(() -> new CustomException(CustomError.WRONG_ID_ERROR));
+        return ComplexDetailRequestDto.fromEntity(complex);
     }
 }
