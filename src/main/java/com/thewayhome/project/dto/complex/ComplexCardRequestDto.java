a50dd6f67313d6692c39a109e622c7b4357ad58d
jykim-rust <alexj99@naver.com>
2023-09-17 14:57:30 +0900

    [JYK] 매물 카드 뷰 DTO

---
+++ src/main/java/com/thewayhome/project/dto/complex/ComplexCardRequestDto.java
@@ -0,0 +1,37 @@
+package com.thewayhome.project.dto.complex;
+
+import com.thewayhome.project.domain.Complex;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ComplexCardRequestDto {
+    private Long articleNo;
+    private String articleName;
+    private String dealOrWarrantPrc;
+    private String tradeTypeName;
+    private String direction;
+    private String realtorName;
+    private String area1;
+    private String area2;
+    private String districtName;
+    private String cortarName;
+
+    public static ComplexCardRequestDto fromEntity(Complex complex){
+        return ComplexCardRequestDto.builder()
+                .articleNo(complex.getArticleNo())
+                .articleName(complex.getArticleName())
+                .dealOrWarrantPrc(complex.getDealOrWarrantPrc())
+                .tradeTypeName(complex.getTradeTypeName())
+                .direction(complex.getDirection())
+                .realtorName(complex.getRealtorName())
+                .area1(complex.getArea1())
+                .area2(complex.getArea2())
+                .districtName(complex.getDistrictName())
+                .cortarName(complex.getCortarName())
+                .build();
+    }
+}
