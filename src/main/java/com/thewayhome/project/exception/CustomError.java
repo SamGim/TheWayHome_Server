e320cae038bca1cad3fbf22fa8fdbaf8b4aef359
jykim-rust <alexj99@naver.com>
2023-09-17 14:56:56 +0900

    [JYK] 잘못된ID조회에러 추가

--- src/main/java/com/thewayhome/project/exception/CustomError.java
+++ src/main/java/com/thewayhome/project/exception/CustomError.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum CustomError {
     TEST_ERROR(404, "테스트 에러입니다."),
     NO_DATA_ERROR(404, "존재하지 않습니다."),
+    WRONG_ID_ERROR(400, "유효한 ID가 아닙니다."),
     DB_SAVE_ERROR(404,"데이터베이스에 저장하지 못했습니다.");
 
 
