88c67dc1053e0b1c0abf0806247e9b11d96ab476
jykim-rust <alexj99@naver.com>
2023-10-01 17:27:54 +0900

    [JYK] 발생할 수 있는 에러 추가

--- src/main/java/com/thewayhome/project/exception/CustomError.java
+++ src/main/java/com/thewayhome/project/exception/CustomError.java
@@ -10,7 +10,13 @@ public enum CustomError {
     NO_DATA_ERROR(404, "존재하지 않습니다."),
     WRONG_ID_ERROR(400, "유효한 ID가 아닙니다."),
     DB_SAVE_ERROR(404,"데이터베이스에 저장하지 못했습니다."),
-    PARSE_ERROR(404, "파싱에 실패했습니다.");
+    PARSE_ERROR(404, "파싱에 실패했습니다."),
+    NAVER_MAPS_API_ERROR(500,"네이버맵스 api 요청결과 200이 아닙니다."),
+    HTTP_PROTOCOL_ERROR(500, "유효하지 않은 HTTP 메소드입니다."),
+    MALFORMED_URL_ERROR(500, "유효하지 않은 URL 입니다."),
+    JSON_PROCESSING_ERROR(500, "JSON 파싱 에러"),
+    JAVA_IO_ERROR(500, "IOException");
+
 
 
     private final int status;
