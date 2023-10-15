50bc260168044e2697a46316e5e4ab19c17af6aa
jykim-rust <alexj99@naver.com>
2023-10-15 12:36:25 +0900

    [JYK] 매물과 직장사이 소요시간을 관리하기위한 도메인 추

---
+++ src/main/java/com/thewayhome/project/domain/CompanyComplexDuration.java
@@ -0,0 +1,23 @@
+package com.thewayhome.project.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "company_complex_duration")
+public class CompanyComplexDuration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ccd_id")
+    private Long ccdId;
+    @Column(name = "min_duration")
+    private Integer min_duration;
+    @Column(name = "max_duration")
+    private Integer max_duration;
+
+}
