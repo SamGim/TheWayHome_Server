86ceb8ee50c2a8cf1666235f2636c2c1b5d76be1
jykim-rust <alexj99@naver.com>
2023-10-17 18:32:49 +0900

    [JYK] 매물을 등록할 때 사용할 DTO 추가

---
+++ src/main/java/com/thewayhome/project/dto/complex/RealComplexRegisterRequestDto.java
@@ -0,0 +1,64 @@
+package com.thewayhome.project.dto.complex;
+
+import com.thewayhome.project.domain.RealComplex;
+import jakarta.persistence.Column;
+import lombok.*;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RealComplexRegisterRequestDto {
+    private Long id;
+    private String tradeTypeName;
+    private String dealOrWarrantPrc;
+    private String maintenanceCost;
+    private String maintenanceCostIncludings;
+    private String structure;
+    private String supplyArea;
+    private String dedicatedArea;
+    private Integer wholeFloor;
+    private Integer floor;
+    private Boolean parkable;
+    private Boolean elevatable;
+    private String options;
+    private String details;
+    private String loan;
+    private Boolean petable;
+    private Double latitude;
+    private Double longitude;
+
+    public RealComplex toEntity() throws ParseException {
+        Point point = (latitude != null && longitude != null)
+                ? (Point) new WKTReader().read(String.format("POINT(%s %s)", latitude, longitude))
+                : null;
+        return RealComplex.builder()
+                .tradeTypeName(this.tradeTypeName)
+                .dealOrWarrantPrc(this.dealOrWarrantPrc)
+                .maintenanceCost(this.maintenanceCost)
+                .maintenanceCostIncludings(this.maintenanceCostIncludings)
+                .structure(this.structure)
+                .supplyArea(this.supplyArea)
+                .dedicatedArea(this.dedicatedArea)
+                .wholeFloor(this.wholeFloor)
+                .floor(this.floor)
+                .parkable(this.parkable)
+                .elevatable(this.elevatable)
+                .options(this.options)
+                .details(this.details)
+                .loan(this.loan)
+                .petable(this.petable)
+                .latitude(this.latitude)
+                .longitude(this.longitude)
+                .location(point)
+                .build();
+    }
+}
+
