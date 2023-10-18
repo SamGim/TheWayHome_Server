576f00a60593a698b3ba14a7117724e02facbf3c
jykim-rust <alexj99@naver.com>
2023-10-18 17:13:45 +0900

    [JYK] 새로 추가한 RealComplex를 반영해 기존의 Complex메소드와 동일한 기능을 하는 메소드들 재구현

--- src/main/java/com/thewayhome/project/repository/RealComplexRepository.java
+++ src/main/java/com/thewayhome/project/repository/RealComplexRepository.java
@@ -1,18 +1,30 @@
 package com.thewayhome.project.repository;
 
+import com.thewayhome.project.domain.Complex;
 import com.thewayhome.project.domain.RealComplex;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
 public interface RealComplexRepository extends JpaRepository<RealComplex, Long> {
+
+    @Query(value = "SELECT * FROM real_complex AS c WHERE MBRCONTAINS(ST_LINESTRINGFROMTEXT(CONCAT('LINESTRING(', :ne_lat, ' ', :ne_lng, ', ', :sw_lat, ' ', :sw_lng, ')')), c.location)", nativeQuery = true)
+    List<RealComplex> findComplexesInBoundingBox(
+            @Param("sw_lng") double swLng,
+            @Param("sw_lat") double swLat,
+            @Param("ne_lng") double neLng,
+            @Param("ne_lat") double neLat
+    );
+
     @Override
     void flush();
 
