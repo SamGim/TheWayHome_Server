6a285c5bde59c5b941f78adc38528e51a4950703
jykim-rust <alexj99@naver.com>
2023-10-18 17:12:28 +0900

    [JYK] 파일이름 통일성 유지를 위해 이름변경

--- src/main/java/com/thewayhome/project/dto/complex/ComplexSimpleResponseDto.java
+++ src/main/java/com/thewayhome/project/dto/complex/ComplexSimpleResponseDto.java
@@ -1,8 +1,6 @@
 package com.thewayhome.project.dto.complex;
 
-import com.thewayhome.project.controller.ComplexController;
 import com.thewayhome.project.domain.Complex;
-import jakarta.persistence.Column;
 import lombok.*;
 
 @Getter
@@ -10,15 +8,15 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ComplexSimpleRequestDto {
+public class ComplexSimpleResponseDto {
     private Long articleNo;
     private String articleName;
     private String dealOrWarrantPrc;
     private Double latitude;
     private Double longitude;
 
-    public static ComplexSimpleRequestDto fromEntity(Complex complex){
-        return ComplexSimpleRequestDto.builder()
+    public static ComplexSimpleResponseDto fromEntity(Complex complex){
+        return ComplexSimpleResponseDto.builder()
                 .articleNo(complex.getArticleNo())
                 .articleName(complex.getArticleName())
                 .dealOrWarrantPrc(complex.getDealOrWarrantPrc())
