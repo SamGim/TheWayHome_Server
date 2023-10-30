51c31152c8a8a02c3ee3b9ead485dd419aa332c8
jykim-rust <alexj99@naver.com>
2023-10-30 14:25:12 +0900

    [JYK] 이미지 업로드를 위한 도커 볼륨 연결

---
+++ src/main/java/com/thewayhome/project/config/ResourceConfig.java
@@ -0,0 +1,16 @@
+package com.thewayhome.project.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:/app/src/main/resources/static/images");
+//        registry.addResourceHandler("/carzenResource/**")
+//                .addResourceLocations("file:/Users/jykim/Desktop/carzenResource/");
+    }
+}
