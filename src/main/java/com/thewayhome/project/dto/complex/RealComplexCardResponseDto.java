36fdc050c1bfd774a01340fc3cec303662a67628
jykim-rust <alexj99@naver.com>
2023-11-12 15:26:21 +0900

    [JYK] RealComplex 리팩토링을 위한 DTO 리팩토링

--- src/main/java/com/thewayhome/project/dto/complex/RealComplexCardResponseDto.java
+++ src/main/java/com/thewayhome/project/dto/complex/RealComplexCardResponseDto.java
@@ -15,37 +15,29 @@ public class RealComplexCardResponseDto {
     private Long id;
     private String name;
     private String tradeTypeName;
-    private String dealOrWarrantPrc;
-    private String maintenanceCost;
-//    private String maintenanceCostIncludings;
-//    private String structure;
+    private Integer duration;
+    private String dealPrc;
+    private String warrantPrc;
     private String supplyArea;
     private String dedicatedArea;
-//    private Integer wholeFloor;
-//    private Integer floor;
-//    private Boolean parkable;
-//    private Boolean elevatable;
-//    private String options;
-//    private String details;
-//    private String loan;
-//    private Boolean petable;
     private String mainImageUrl;
-//    private List<ComplexImageResponseDto> roomImages;
     private Double latitude;
     private Double longitude;
 
-    public static RealComplexCardResponseDto fromEntity(RealComplex complex) {
-
+    public static RealComplexCardResponseDto fromEntity(RealComplex complex, Integer duration){
         return RealComplexCardResponseDto.builder()
                 .id(complex.getId())
+                .name(complex.getName())
                 .tradeTypeName(complex.getTradeTypeName())
-                .dealOrWarrantPrc(complex.getDealOrWarrantPrc())
-                .maintenanceCost(complex.getMaintenanceCost())
+                .duration(duration)
+                .dealPrc(complex.getDealPrc())
+                .warrantPrc(complex.getWarrantPrc())
                 .supplyArea(complex.getSupplyArea())
                 .dedicatedArea(complex.getDedicatedArea())
                 .mainImageUrl(complex.getMainImageUrl())
                 .latitude(complex.getLatitude())
                 .longitude(complex.getLongitude())
                 .build();
+
     }
 }
