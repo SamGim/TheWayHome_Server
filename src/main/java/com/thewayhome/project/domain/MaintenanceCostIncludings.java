7397da5e6bebab5b97d8134866b1cd53af1954ce
jykim-rust <alexj99@naver.com>
2023-11-12 15:29:04 +0900

    [JYK] RealComplex 리팩토링을 위한 MaintenanceCostIncludings 생성

---
+++ src/main/java/com/thewayhome/project/domain/MaintenanceCostIncludings.java
@@ -0,0 +1,35 @@
+package com.thewayhome.project.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "maintenance_cost_includings")
+public class MaintenanceCostIncludings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "electricity", nullable = false)
+    private Boolean electricity;
+
+    @Column(name = "gas", nullable = false)
+    private Boolean gas;
+
+    @Column(name = "internet", nullable = false)
+    private Boolean internet;
+
+    @Column(name = "water", nullable = false)
+    private Boolean water;
+
+    @Column(name = "tv", nullable = false)
+    private Boolean tv;
+
+}
