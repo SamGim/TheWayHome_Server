19eba26933913f8d679d4445c6f28d0074c0c571
jykim-rust <alexj99@naver.com>
2023-11-12 15:26:25 +0900

    [JYK] RealComplex 리팩토링을 위한 DTO 리팩토링

--- src/main/java/com/thewayhome/project/dto/complex/RealComplexRegisterRequestDto.java
+++ src/main/java/com/thewayhome/project/dto/complex/RealComplexRegisterRequestDto.java
@@ -1,7 +1,8 @@
 package com.thewayhome.project.dto.complex;
 
-import com.thewayhome.project.domain.RealComplex;
-import jakarta.persistence.Column;
+import com.thewayhome.project.domain.*;
+import com.thewayhome.project.dto.complexEtc.*;
+import jakarta.persistence.*;
 import lombok.*;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.io.ParseException;
@@ -17,47 +18,124 @@ import java.util.List;
 @Builder
 public class RealComplexRegisterRequestDto {
     private String name;
+
+    // 거래유형(전세, 월세, 매매)
     private String tradeTypeName;
-    private String dealOrWarrantPrc;
-    private String maintenanceCost;
-    private String maintenanceCostIncludings;
-    private String structure;
+
+    // 주소
+    private String address;
+
+    // 상세주소
+    private String detailAddress;
+
+    // 공급면적(단위 m^2)
     private String supplyArea;
+
+    // 전용면적(단위 m^2)
     private String dedicatedArea;
+
+    // 구조(오픈형 원룸, 분리형 원룸, 복층형 원룸, 투룸, 쓰리룸, 포룸+)
+    private String structure;
+
+    // 임대료
+    private String dealPrc;
+
+    // 보증금
+    private String warrantPrc;
+
+    // 관리비(단위: 만원)
+    private String maintenanceCost;
+
+    // 관리비 포함내역(전기세, 가스, 인터넷, 수도, TV)
+    private MaintenanceCostIncludingsDto maintenanceCostIncludings;
+
+    // 입주가능일
+    private String availableDate;
+
+    // 입주협가능여부
+    private Boolean availableDateNegotiable;
+
+    // 전체층
     private Integer wholeFloor;
+
+    // 해당층
     private Integer floor;
-    private Boolean parkable;
-    private Boolean elevatable;
-    private String options;
-    private String details;
-    private String loan;
-    private Boolean petable;
+
     private Double latitude;
+
     private Double longitude;
+
+
+    // 여기서부터 옵션
+    // 주차가능
+    private Boolean parkable;
+
+    // 엘리베이터 여부
+    private Boolean elevatable;
+
+    // 옵션(싱크대, 에어컨, 세탁기, ...etc)
+    private String options;
+
+    // 매물 설명
+    private String details;
+
+    // 대출 여부(LH전세대출 가능, 카카오뱅크 전세대출 가능)
+    private String loan;
+
+    // 펫 여부
+    private Boolean petable;
+
+    // 욕실수
+    private Integer bathroomCount;
+
+    // 시설정보옵션들
+    // 난방시설
+    private String heatingFacility;
+
+    // 냉방시설
+    private CoolingFacilityDto coolingFacility;
+
+    // 생활시설
+    private LivingFacilityDto livingFacility;
+
+    // 보안시설
+    private SecurityFacilityDto securityFacility;
+
+    // 기타시설
+    private EtcFacilityDto etcFacility;
+
+    private String description;
 
     public RealComplex toEntity() throws ParseException {
         Point point = (latitude != null && longitude != null)
                 ? (Point) new WKTReader().read(String.format("POINT(%s %s)", latitude, longitude))
                 : null;
         return RealComplex.builder()
-                .name(this.name)
-                .tradeTypeName(this.tradeTypeName)
-                .dealOrWarrantPrc(this.dealOrWarrantPrc)
-                .maintenanceCost(this.maintenanceCost)
-                .maintenanceCostIncludings(this.maintenanceCostIncludings)
-                .structure(this.structure)
-                .supplyArea(this.supplyArea)
-                .dedicatedArea(this.dedicatedArea)
-                .wholeFloor(this.wholeFloor)
-                .floor(this.floor)
-                .parkable(this.parkable)
-                .elevatable(this.elevatable)
-                .options(this.options)
-                .details(this.details)
-                .loan(this.loan)
-                .petable(this.petable)
-                .latitude(this.latitude)
-                .longitude(this.longitude)
+                .name(name)
+                .tradeTypeName(tradeTypeName)
+                .address(address)
+                .detailAddress(detailAddress)
+                .supplyArea(supplyArea)
+                .dedicatedArea(dedicatedArea)
+                .structure(structure)
+                .dealPrc(dealPrc)
+                .warrantPrc(warrantPrc)
+                .maintenanceCost(maintenanceCost)
+                .availableDate(availableDate)
+                .availableDateNegotiable(availableDateNegotiable)
+                .wholeFloor(wholeFloor)
+                .floor(floor)
+                .latitude(latitude)
+                .longitude(longitude)
+                .parkable(parkable)
+                .elevatable(elevatable)
+                .options(options)
+                .details(details)
+                .loan(loan)
+                .petable(petable)
+                .bathroomCount(bathroomCount)
+                .heatingFacility(heatingFacility)
+                .description(description)
                 .location(point)
                 .build();
     }
