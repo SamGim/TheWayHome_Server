a31fb111b81909e9c12b7c8b0b26031ace117971
jykim-rust <alexj99@naver.com>
2023-11-12 15:29:40 +0900

    [JYK] RealComplex 리팩토링을 위한 SecurityFacilityRepo 생성

---
+++ src/main/java/com/thewayhome/project/repository/SecurityFacilityRepository.java
@@ -0,0 +1,7 @@
+package com.thewayhome.project.repository;
+
+import com.thewayhome.project.domain.SecurityFacility;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SecurityFacilityRepository extends JpaRepository<SecurityFacility, Long> {
+}
