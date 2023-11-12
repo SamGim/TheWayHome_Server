eb646023af5a8781de8c555462d3a2509f484c09
jykim-rust <alexj99@naver.com>
2023-11-12 15:28:47 +0900

    [JYK] RealComplex 리팩토링을 위한 LivingFacility 생성

---
+++ src/main/java/com/thewayhome/project/domain/LivingFacility.java
@@ -0,0 +1,72 @@
+package com.thewayhome.project.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "living_facility")
+public class LivingFacility {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    // 에어컨
+    @Column(name = "air_conditioner", columnDefinition = "boolean default false")
+    private Boolean airConditioner;
+
+    // 냉장고
+    @Column(name = "refrigerator", columnDefinition = "boolean default false")
+    private Boolean refrigerator;
+
+    // 세탁기
+    @Column(name = "washing_machine", columnDefinition = "boolean default false")
+    private Boolean washingMachine;
+
+    // 가스레인지
+    @Column(name = "gas_range", columnDefinition = "boolean default false")
+    private Boolean gasRange;
+
+    // 전자레인지
+    @Column(name = "microwave", columnDefinition = "boolean default false")
+    private Boolean microwave;
+
+    // 인덕션
+    @Column(name = "induction", columnDefinition = "boolean default false")
+    private Boolean induction;
+
+    // 책상
+    @Column(name = "desk", columnDefinition = "boolean default false")
+    private Boolean desk;
+
+    // 책장
+    @Column(name = "bookshelf", columnDefinition = "boolean default false")
+    private Boolean bookshelf;
+
+    // 식탁
+    @Column(name = "dining_table", columnDefinition = "boolean default false")
+    private Boolean diningTable;
+
+    // 의자
+    @Column(name = "chair", columnDefinition = "boolean default false")
+    private Boolean chair;
+
+    // 침대
+    @Column(name = "bed", columnDefinition = "boolean default false")
+    private Boolean bed;
+
+    // 옷장
+    @Column(name = "closet", columnDefinition = "boolean default false")
+    private Boolean closet;
+
+    // 건조기
+    @Column(name = "dryer", columnDefinition = "boolean default false")
+    private Boolean dryer;
+}
---
+++ src/main/java/com/thewayhome/project/repository/LivingFacilityRepository.java
@@ -0,0 +1,7 @@
+package com.thewayhome.project.repository;
+
+import com.thewayhome.project.domain.LivingFacility;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivingFacilityRepository extends JpaRepository<LivingFacility, Long> {
+}
