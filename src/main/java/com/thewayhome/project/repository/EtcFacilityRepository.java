7eeabe5b7810d1a462726a5e177e39e31dea9311
jykim-rust <alexj99@naver.com>
2023-11-12 15:28:27 +0900

    [JYK] RealComplex 리팩토링을 위한 EtcFacilityRepository 생성

---
+++ src/main/java/com/thewayhome/project/repository/EtcFacilityRepository.java
@@ -0,0 +1,7 @@
+package com.thewayhome.project.repository;
+
+import com.thewayhome.project.domain.EtcFacility;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EtcFacilityRepository extends JpaRepository<EtcFacility, Long> {
+}
