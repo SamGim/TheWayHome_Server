b3faa94d46eba3202d456cb89621c1183c5b6830
jykim-rust <alexj99@naver.com>
2023-11-12 15:29:16 +0900

    [JYK] RealComplex 리팩토링을 위한 MaintenanceCostIncludingsRepo 생성

---
+++ src/main/java/com/thewayhome/project/repository/MaintenanceCostIncludingsRepository.java
@@ -0,0 +1,7 @@
+package com.thewayhome.project.repository;
+
+import com.thewayhome.project.domain.MaintenanceCostIncludings;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MaintenanceCostIncludingsRepository extends JpaRepository<MaintenanceCostIncludings, Long> {
+}
