5b5e8c66696a9ba4a69a11c1ad6fa975e59038cb
jykim-rust <alexj99@naver.com>
2023-11-12 15:27:31 +0900

    [JYK] BaseEntity 구현

---
+++ src/main/java/com/thewayhome/project/domain/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.thewayhome.project.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "updated_by", nullable = false)
+    private String updatedBy;
+}
