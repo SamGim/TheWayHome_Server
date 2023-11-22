086d89379f9ced4b5e6ff3450d6a9d2a442552a7
jykim-rust <alexj99@naver.com>
2023-11-22 13:05:33 +0900

    [JYK] 보안 시설 선택지 변경 및 추가

--- src/main/java/com/thewayhome/project/domain/SecurityFacility.java
+++ src/main/java/com/thewayhome/project/domain/SecurityFacility.java
@@ -17,20 +17,36 @@ public class SecurityFacility {
     @Column(name = "id")
     private Long id;
 
-    // 출입문 보안
-    @Column(name = "door_security", columnDefinition = "boolean default false")
-    private Boolean doorSecurity;
+    // 사설 경비원
+    @Column(name = "private_security_guard", columnDefinition = "boolean default false")
+    private Boolean privateSecurityGuard;
 
     // CCTV
     @Column(name = "cctv", columnDefinition = "boolean default false")
     private Boolean cctv;
 
-    // 현관보안
+    // 공동현관보안
     @Column(name = "entrance_security", columnDefinition = "boolean default false")
     private Boolean entranceSecurity;
 
     // 경비원
     @Column(name = "security_guard", columnDefinition = "boolean default false")
     private Boolean securityGuard;
+
+    // 비디어폰
+    @Column(name = "video_phone", columnDefinition = "boolean default false")
+    private Boolean videoPhone;
+
+    // 인터폰
+    @Column(name = "inter_Phone", columnDefinition = "boolean default false")
+    private Boolean interPhone;
+
+    // 원룸도어락
+    @Column(name = "one_room_door_lock", columnDefinition = "boolean default false")
+    private Boolean oneRoomDoorLock;
+
+    // 창문 방법창
+    @Column(name = "window_method_window", columnDefinition = "boolean default false")
+    private Boolean windowMethodWindow;
 
 }
--- src/main/java/com/thewayhome/project/dto/complexEtc/SecurityFacilityDto.java
+++ src/main/java/com/thewayhome/project/dto/complexEtc/SecurityFacilityDto.java
@@ -1,7 +1,6 @@
 package com.thewayhome.project.dto.complexEtc;
 
 import com.thewayhome.project.domain.SecurityFacility;
-import jakarta.persistence.Column;
 import lombok.*;
 
 @Getter
@@ -10,26 +9,38 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class SecurityFacilityDto {
-    private Boolean doorSecurity;
+    private Boolean privateSecurityGuard;
     private Boolean cctv;
     private Boolean entranceSecurity;
     private Boolean securityGuard;
+    private Boolean videoPhone;
+    private Boolean interPhone;
+    private Boolean oneRoomDoorLock;
+    private Boolean windowMethodWindow;
 
     public static SecurityFacilityDto fromEntity(SecurityFacility securityFacility){
         return SecurityFacilityDto.builder()
-                .doorSecurity(securityFacility.getDoorSecurity())
+                .privateSecurityGuard(securityFacility.getPrivateSecurityGuard())
                 .cctv(securityFacility.getCctv())
                 .entranceSecurity(securityFacility.getEntranceSecurity())
                 .securityGuard(securityFacility.getSecurityGuard())
+                .videoPhone(securityFacility.getVideoPhone())
+                .interPhone(securityFacility.getInterPhone())
+                .oneRoomDoorLock(securityFacility.getOneRoomDoorLock())
+                .windowMethodWindow(securityFacility.getWindowMethodWindow())
                 .build();
     }
 
     public SecurityFacility toEntity() {
         return SecurityFacility.builder()
-                .doorSecurity(this.doorSecurity)
+                .privateSecurityGuard(this.privateSecurityGuard)
                 .cctv(this.cctv)
                 .entranceSecurity(this.entranceSecurity)
                 .securityGuard(this.securityGuard)
+                .videoPhone(this.videoPhone)
+                .interPhone(this.interPhone)
+                .oneRoomDoorLock(this.oneRoomDoorLock)
+                .windowMethodWindow(this.windowMethodWindow)
                 .build();
     }
 }
