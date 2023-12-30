95d96a4c435448a789175b13733cbe1d40d180d7
jykim-rust <alexj99@naver.com>
2023-12-30 14:16:32 +0900

    :sparkles: 배치서버와 연결을 위한 DTO추가

---
+++ src/main/java/com/thewayhome/project/dto/complex/ComplexTimeDto.java
@@ -0,0 +1,13 @@
+package com.thewayhome.project.dto.complex;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ComplexTimeDto {
+    private Long id;
+    private Integer duration;
+}
